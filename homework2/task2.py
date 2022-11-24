4ea239bba056da0be2a60fba7977c9be0430e385
smilezz <lihachev647@gmail.com>
2022-11-24 20:23:33 +0300

    do task2

--- homework2/task2.py
+++ homework2/task2.py
@@ -1,3 +1,20 @@
 #2) Вводим с клавиатуры целое число X
 #Далее вводим Х целых чисел.
 #Необходимо вывести на экран максимальное и второе максимальное число из введенных чисел.
+
+count_numbers = int(input('Write how much numbers you were write: '))
+max_number = None
+second_max = None
+
+while count_numbers > 0:
+    number = (int(input('Write the number: ')))
+    if max_number is None:
+        max_number = number
+    elif number > max_number:
+        second_max = max_number
+        max_number = number
+    elif second_max is None:
+        second_max = number
+    count_numbers -= 1
+
+print(f'Max number is {max_number}, second max number is {second_max}')
