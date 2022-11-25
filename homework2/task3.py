baf8624ea8a8a110549ad053c0c6f741a8a92894
smilezz <lihachev647@gmail.com>
2022-11-25 03:17:18 +0300

    do the second option for task 3

--- homework2/task3.py
+++ homework2/task3.py
@@ -5,18 +5,28 @@
 salary = int(input('Write salary: '))
 salary_help = salary
 bills = [0, 0, 0, 0]
-while salary_help != 0:
-    if salary_help % 25 == 0:
-        bills[0] += 1
-        salary_help -= 25
-    elif salary_help % 10 == 0:
-        bills[1] += 1
-        salary_help -= 10
-    elif salary_help % 5 == 0:
-        bills[2] += 1
-        salary_help -= 5
-    else:
-        bills[3] += 1
-        salary_help -= 1
+
+#option 1
+# while salary_help != 0:
+#     if salary_help % 25 == 0:
+#         bills[0] += 1
+#         salary_help -= 25
+#     elif salary_help % 10 == 0:
+#         bills[1] += 1
+#         salary_help -= 10
+#     elif salary_help % 5 == 0:
+#         bills[2] += 1
+#         salary_help -= 5
+#     else:
+#         bills[3] += 1
+#         salary_help -= 1
+
+#option 2
+i = 0
+for j in 25, 10, 5, 1:
+    bills[i] = salary_help // j
+    salary_help -= bills[i] * j
+    if salary_help == 0: break
+    i += 1
 
 print(f'Salary {salary} can be paid as: 25 - {bills[0]}, 10 - {bills[1]}, 5 - {bills[2]}, 1 - {bills[3]}')
