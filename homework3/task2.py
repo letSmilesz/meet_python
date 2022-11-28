be75895979272c0d3de0dcd61a60de2c4ddde265
smilezz <lihachev647@gmail.com>
2022-11-28 18:35:14 +0300

    do task2

--- homework3/task2.py
+++ homework3/task2.py
@@ -3,3 +3,18 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+import functions
+
+def find_multiplication_on_pairs(list):
+    result = []
+    count = len(list) // 2 if len(list) % 2 == 0 else len(list) // 2 + 1
+    for i in range(count):
+        result.append(list[i] * list[len(list) - 1 - i])
+    return result
+
+length, min_el, max_el = functions.ask_parameters_to_list()
+numbers = functions.create_list_of_numbers(length, min_el, max_el)
+res = find_multiplication_on_pairs(numbers)
+print(numbers)
+print(res)
