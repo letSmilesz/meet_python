a32065e066453f0166a32f9fc5179aef4cca86d0
smilezz <lihachev647@gmail.com>
2022-11-28 19:32:04 +0300

    do task 4

--- homework3/task4.py
+++ homework3/task4.py
@@ -3,3 +3,13 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+def convert_to_binary(num):
+    answer = ""
+    while num > 0:
+        answer = str(num % 2) + answer
+        num //= 2
+    return answer
+
+number = int(input('Enter the number you want to convert to binary: '))
+print(convert_to_binary(number))
