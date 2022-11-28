8f0f50322988d3d1879b134b97a3b14ca473bd73
smilezz <lihachev647@gmail.com>
2022-11-28 19:40:21 +0300

    do task4 with recursion

--- homework3/task4.py
+++ homework3/task4.py
@@ -11,5 +11,10 @@ def convert_to_binary(num):
         num //= 2
     return answer
 
+def recursion_convert_to_binary(num):
+    if num == 0: return ""
+    else: return recursion_convert_to_binary(num // 2) + str(num % 2)
+
 number = int(input('Enter the number you want to convert to binary: '))
 print(convert_to_binary(number))
+print(recursion_convert_to_binary(number))
