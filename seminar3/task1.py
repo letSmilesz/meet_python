809956f30fcbca78410d8623478c6a4150cd2a23
smilezz <lihachev647@gmail.com>
2022-11-29 17:31:13 +0300

    do task2

--- seminar3/task1.py
+++ seminar3/task1.py
@@ -1,1 +1,12 @@
 # 3.1 Вводим с клавиатуры десятичное число. Необходимо перевести его  в двоичную систему счисления.
+num = int(input('Write number: '))
+
+def to_binary(num):
+    ans = ''
+    while num > 0:
+        help = num % 2
+        ans = str(help) + ans
+        num //= 2
+    return ans
+
+print(to_binary(num))
--- seminar3/task2.py
+++ seminar3/task2.py
@@ -1,1 +1,13 @@
 # 3.2 Вводим любую строку текста (на русском). Необходимо посчитать количество гласных и согласных букв.
+
+text = input('Write some text in English: ')
+vowels_letters = ['e', 'y', 'u', 'i', 'o', 'a']
+text = text.lower()
+text = text.replace(' ', '')
+vowels = 0
+for i in vowels_letters:
+    vowels += text.count(i)
+consonants = len(text) - vowels
+print(text)
+print(vowels)
+print(consonants)
