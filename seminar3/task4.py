fd8faf995a273599ee48f37c81646395dcdff178
smilezz <lihachev647@gmail.com>
2022-11-29 17:58:45 +0300

    do task 4

--- seminar3/task4.py
+++ seminar3/task4.py
@@ -1,1 +1,9 @@
 # 3.4 Напишем программу, которая из введённой строки пользователя, поделит её пополам и поменяет половинки местами
+
+def replace_half_text(text: str):
+    half = len(text) // 2
+    if len(text) % 2 != 0: half += 1
+    return text[half: len(text)] + text[0: half]
+
+text = input('Enter some text: ')
+print(replace_half_text(text))
