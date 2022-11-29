a4f6366738623573162730ede61f32406857deef
smilezz <lihachev647@gmail.com>
2022-11-29 15:25:58 +0300

    fixed task 3

--- homework3_v2/task3.py
+++ homework3_v2/task3.py
@@ -5,7 +5,8 @@
 def reverse_between_letter(text: str, letter):
     start = text.find(letter)
     end = text.rfind(letter)
-    return text[0:start] + text[end:start:-1] + text[end:len(text)]
+    if start == end: return (f'In this text only one letter "{letter}"')
+    else: return text[0:start] + text[end:start:-1] + text[end:len(text)]
 
 text = input('Enter the text: ')
 print(reverse_between_letter(text.lower(), 'o'))
