61f4d3cf3c446fa63d878bd4431980bdec31de43
smilezz <lihachev647@gmail.com>
2022-11-29 19:42:58 +0300

    do task 7

--- seminar3/task7.py
+++ seminar3/task7.py
@@ -1,1 +1,12 @@
 # 3.7 Вводим строку с клавиатуры. Необходимо сказать, какой символ встречается чаще всего
+
+text = input('Enter some text: ')
+letters = {}
+for i in range(len(text)):
+    try:
+        letters[text[i]] += 1
+    except KeyError: letters[text[i]] = 1
+letter_win = text[0]
+for i in letters:
+    if letters[i] > letters[letter_win]: letter_win = i
+print(f'The "{letter_win}" is most popular in this text. It meets {letters[letter_win]} counts.') 
