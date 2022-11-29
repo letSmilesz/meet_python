dd349b7b1a14f968cd5e597079299faacb8c4caa
smilezz <lihachev647@gmail.com>
2022-11-29 15:11:58 +0300

    do task3

--- homework3_v2/task2.py
+++ homework3_v2/task2.py
@@ -1,1 +1,15 @@
 #3.11 Вводим с клавиатуры строку. Необходимо сказать, является ли эта строка дробным числом
+
+def is_float(txt):
+    try:
+        float(txt)
+        return True
+    except ValueError: return False
+
+text = input('Enter the text or number: ')
+if is_float(text):
+    num = float(text)
+    if num % 1 == 0:
+        num = int(text)
+    print(f'You`re enter {type(num)} type.')
+else: print('You`re enter string')
