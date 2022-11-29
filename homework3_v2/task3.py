76ac2f7ae03582618d5dae827d4f392a02fb3dbe
smilezz <lihachev647@gmail.com>
2022-11-29 15:19:39 +0300

    do task 3

--- homework3_v2/task3.py
+++ homework3_v2/task3.py
@@ -1,3 +1,11 @@
 # 3.12 Вводим с клаиватуры строку. Необходимо вывести строку, где развернём подстроку между первой 
 # и последней буквой "о" из исходной строки. Если она только одна или её нет - то сообщить, что буква 
 # "о" - одна или не встречается.
+
+def reverse_between_letter(text: str, letter):
+    start = text.find(letter)
+    end = text.rfind(letter)
+    return text[0:start] + text[end:start:-1] + text[end:len(text)]
+
+text = input('Enter the text: ')
+print(reverse_between_letter(text.lower(), 'o'))
