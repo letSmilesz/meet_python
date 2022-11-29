3d89ff4ed4f48e894aa0b8773d4fcb0af6581897
smilezz <lihachev647@gmail.com>
2022-11-29 18:16:43 +0300

    do task 5

--- seminar3/task5.py
+++ seminar3/task5.py
@@ -1,1 +1,7 @@
 # 3.5 Вводим любую строку и нужно посчитать кол-во символов в верхнем регистре
+
+text = input('Enter some text: ')
+count = 0
+for i in range(len(text)):
+    if text[i].isupper(): count += 1
+print(count)
