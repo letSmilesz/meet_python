87855470ec82ee9ebc466bf74de8c77aff9ac923
smilezz <lihachev647@gmail.com>
2022-11-29 17:44:16 +0300

    do task 3

--- seminar3/task3.py
+++ seminar3/task3.py
@@ -1,1 +1,5 @@
 # 3.3 Вводим любое слово\словочетание - определить, является ли оно палиндромом
+text = input('Enter a word or phrase: ')
+text.lower()
+if text == text[:: -1]: print('It`s palindrom!')
+else: print('It isn`t palindrom')
