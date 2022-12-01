b8651c841185920a7804c797a32d82d0c1a04a0d
smilezz <lihachev647@gmail.com>
2022-12-01 21:48:19 +0300

    do all tasks

--- seminar4/task3.py
+++ seminar4/task3.py
@@ -3,3 +3,17 @@
 #     *Пример:*
 #     - [2, 3, 4, 5, 6] => [12, 15, 4];
 #     - [2, 3, 5, 6] => [12, 15]
+import random
+
+nums = []
+for i in range(5):
+    nums.append(random.randint(0, 15))
+
+res = []
+for i in range((len(nums) // 2) + len(nums) % 2):
+    if nums[i] == nums[len(nums) -1 - i]: res.append(nums[i])
+    else: res.append(nums[i] * nums[len(nums) -1 - i])
+
+
+print(nums)
+print(res)
--- seminar4/task4.py
+++ seminar4/task4.py
@@ -1,3 +1,16 @@
 # 4) Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 #     *Пример:*
 #     - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+num = int(input('Enter the number: '))
+fib = []
+fib.append(0)
+fib.append(1)
+for i in range(2, num + 1):
+    fib.append(fib[i - 1] + fib[i - 2])
+
+fib.insert(0, 1)
+for i in range(1, num):
+    fib.insert(0, fib[1] - fib[0])
+
+print(fib)
