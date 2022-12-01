cddc7c2bef4d79d266b7cc0e0167fdb0a26ef296
smilezz <lihachev647@gmail.com>
2022-12-01 22:37:11 +0300

    do task 4 better a little

--- seminar4/task4.py
+++ seminar4/task4.py
@@ -3,9 +3,9 @@
 #     - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 num = int(input('Enter the number: '))
-fib = []
-fib.append(0)
-fib.append(1)
+fib = [0, 1]
+# fib.append(0)
+# fib.append(1)
 for i in range(2, num + 1):
     fib.append(fib[i - 1] + fib[i - 2])
 
