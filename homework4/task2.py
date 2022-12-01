3dc8dfa64fbbb9895f3d28a505163cadd6bcbdb8
smilezz <lihachev647@gmail.com>
2022-12-02 01:42:56 +0300

    fixed task 2

--- homework4/task2.py
+++ homework4/task2.py
@@ -2,17 +2,17 @@
 # И считаем средне-арифметическое каждой строки (не пользуемся встроенными методами подсчета суммы)
 import random
 
-length = 10#int(input('Enter length of the table: '))
-height = 10#int(input('Enter height of the table: '))
+length = int(input('Enter length of the table: '))
+height = int(input('Enter height of the table: '))
 
 table = []
-sum_lines = []
-for i in range(length):
+avg_lines = []
+for i in range(height):
     table.append(list())
     summ = 0
-    for j in range(height):
+    for j in range(length):
         table[i].append(random.randint(0, 10))
         summ += table[i][j]
-    sum_lines.append(summ)
-    print(f'{table[i]} - sum is {sum_lines[i]}')
+    avg_lines.append(summ // length)
+    print(f'{table[i]} - average is {avg_lines[i]}')
 
