b476f9c337112ef7107cd881d0250ee484cb8cb0
smilezz <lihachev647@gmail.com>
2022-12-01 22:17:28 +0300

    do task1

--- homework4/task1.py
+++ homework4/task1.py
@@ -2,3 +2,16 @@
 # стоящих на нечётной позиции.
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+import random
+
+lenth = int(input('Enter the length of list: '))
+nums = []
+for i in range(lenth):
+    nums.append(random.randint(0, 10))
+
+summ = 0
+for i in range(1, len(nums), 2):
+    summ += nums[i]
+
+print(nums)
+print(summ)
