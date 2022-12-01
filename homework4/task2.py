1077ec62438bfe6a7fc9e3fc141f64013c5f275b
smilezz <lihachev647@gmail.com>
2022-12-01 22:28:55 +0300

    do task 2

--- homework4/task2.py
+++ homework4/task2.py
@@ -1,2 +1,18 @@
 # 2) Написать программу, которая генерирует двумерный массив на A x B элементов ( A и B вводим с клавиатуры)
 # И считаем средне-арифметическое каждой строки (не пользуемся встроенными методами подсчета суммы)
+import random
+
+length = 10#int(input('Enter length of the table: '))
+height = 10#int(input('Enter height of the table: '))
+
+table = []
+sum_lines = []
+for i in range(length):
+    table.append(list())
+    summ = 0
+    for j in range(height):
+        table[i].append(random.randint(0, 10))
+        summ += table[i][j]
+    sum_lines.append(summ)
+    print(f'{table[i]} - sum is {sum_lines[i]}')
+
