65a76831c5939f924fbd97a4a5bbadaf1a35ccc3
smilezz <lihachev647@gmail.com>
2022-12-01 22:36:21 +0300

    do task 3

--- homework4/task3.py
+++ homework4/task3.py
@@ -1,1 +1,15 @@
 # 3) Сгенерируйте список на 30 элементов. Отсортируйте список по возрастанию, методом сортировки выбором.
+import random
+
+nums = []
+for i in range(30):
+    nums.append(random.randint(0,100))
+print(nums)
+
+for i in range(len(nums)):
+    min_index = i
+    for j in range(i, len(nums)):
+        if nums[j] < nums[min_index]: min_index = j
+    nums[i], nums[min_index] = nums[min_index], nums[i]
+print(nums)
+print(len(nums))
