2509cc1bcff2277fc0dd34eeff97f912bbfaf3ae
letSmiles <lihachev647@gmail.com>
2022-12-09 13:31:43 +0300

    do task1

--- seminar6.py/task1.py
+++ seminar6.py/task1.py
@@ -1,1 +1,16 @@
 #Проверить правильную скобочную последовательность
+
+def check(txt: str):
+    if txt.count('(') != txt.count(')'): return False
+    else:
+        parentheses = 0
+        for i in txt:
+            if i == ')': parentheses += 1
+            elif i == '(': parentheses += 1
+            if parentheses < 0: return False
+        else: return True
+
+string = input('Enter string: ')
+ans = check(string)
+if ans == True: print('Normal')
+else: print('Bad')
