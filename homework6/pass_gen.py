6f4079ac0acc7aff1c526552b7bfb8f000fcb954
letSmiles <lihachev647@gmail.com>
2022-12-11 13:57:41 +0300

    small corrections

--- homework6/pass_gen.py
+++ homework6/pass_gen.py
@@ -13,15 +13,14 @@ def generate_password(m, letters):
     for i in range(m):
         what_is = randint(0, 2)
         what_was.append(what_is)
-        if i == m - 3 and (0 in what_was) < 1 or (1 in what_was) < 1 or (2 in what_was) < 1:
-            if (0 in what_was) < 1: what_is = 0
-            elif (1 in what_was) < 1: what_is = 1
+        if i == m - 3 and 0 not in what_was or 1 not in what_was or 2 not in what_was:
+            if 0 not in what_was: what_is = 0
+            elif 1 not in what_was: what_is = 1
             else: what_is = 2
             what_was[len(what_was) - 1] = what_is 
         if what_is == 0: password += str(randint(2, 9))
-        elif what_is == 1: 
-            char = str(choice(letters))
-            password += char.upper()
+        elif what_is == 1:  
+            password += (str(choice(letters))).upper()
         else: password += choice(letters)
     return password
 
