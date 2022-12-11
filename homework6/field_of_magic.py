e69cd959647cc6bc14baa237adeb1c25b775a865
letSmiles <lihachev647@gmail.com>
2022-12-11 11:23:29 +0300

    do the game

--- homework6/field_of_magic.py
+++ homework6/field_of_magic.py
@@ -31,11 +31,11 @@ def animation(txt: str, bool = False, f = plug, word = '', know_letters = [], po
 
 def animation2(word, know_letters, letter):
     clear()
-    for i in range(word):
+    for i in range(len(word)):
             if word[i] == letter:
-                for j in range(2):
+                for j in range(3):
                     print_game(word, know_letters, '', points)
-                    know_letters[i] += 1
+                    if j < 2: know_letters[i] += 1
                     sleep(1)
                     clear()
 
@@ -47,24 +47,25 @@ def print_rules(theme):
     print(f'Rules: \nThe theme of game is "{theme}"!\nYour task is as soon as possible guess the word!'
     + 'You have 15 attempts.\nYou must enter the letter that you think is present in the word. '
     + '\nEach turn the drum will spin, which will show how many points you get for guessing the '
-    + '\nletter, and there is also a chance to get a bonus sector! \nTo exit the game, leave the line '
-    + 'blank and press "Enter". \n\nTo continue press "Enter"')
+    + '\nletter, and there is also a chance to get a bonus sector! \nAlso, if you know the word, you '
+    + 'can enter in its entirety! \nTo exit the game, leave the line blank and press "Enter".'
+    + ' \n\nTo continue press "Enter"')
     input()
     clear()
 
 def drum(turn):
     values = [1, 1, 1, 1, 1, 3, 3, 3, 3, 5, 5, 5, 5, 8, 8, 8, 10, 15, 30, 40, 50, 60, 70, 80, 90, 100, 999]
-    prizes = ['x', 'b', '+']
+    prizes = ['x', 'b', 'p']
     animation('The drum is revolving.')
     if turn > 3: 
-        value = values[randint(0, len(values) - 2)]
+        value = values[randint(0, len(values) - 1)]
         if value == 999: value = choice(prizes)
-    else: value = values[randint(0, len(values) - 1)]
-    if value is int: print(f'You will have {value} points if you quess the letter!')
-    elif value == 'x': print('Lucky! How many letters you guess - so many times the number of \
-        points will increase!')
+    else: value = values[randint(0, len(values) - 2)]
+    if value == 'p': print('You have sector "Plus", you can open any letter!')
+    elif value == 'x': print('Lucky! How many letters you guess - so many times the number of'
+        + 'points will increase!')
     elif value == 'b': print('Bad luck :(\nYou caught sector "Bankrupt". All your points are gone')
-    else: print('You have sector "Plus", you can open any letter!')
+    else: print(f'You will have {value} points if you quess the letter!')
     return value
 
 def end_of_game(bool, word, know_letters, points):
@@ -84,6 +85,7 @@ def check_num_letter(num_letter, word, know_letters):
 dictionary = {'art': ('brightness', 'illustration'), 'birds': ('flycatcher', 'grosbeak'),
     'computers': ('cyberspace', 'development'), 'driving': ('expressway', 'cloverleaf')}
 themes = ['art', 'birds', 'computers', 'driving']
+entered_letters = []
 theme = choice(themes)
 word = str(dictionary.get(theme)[randint(0,1)])
 know_letters = create_list(word)
@@ -98,7 +100,10 @@ for i in range(15):
         num_letter = check_num_letter(num_letter, word, know_letters)
         animation2(word, know_letters, word[num_letter - 1])
         print_game(word, know_letters, theme, points)
-    letter = input('Which letter do you choose? Enter it: ')
+    if value == 'b': 
+        points = 0
+        continue
+    else: letter = input('Which letter do you choose? Enter it: ')
     if len(letter) > 1: 
         answer = input('Are you sure you want to enter the whole word? Enter "yes" or "no": ').lower()
         if answer == 'yes': 
@@ -117,14 +122,19 @@ for i in range(15):
         clear()
         print('You are out of the game :(')
         break
+    if letter in entered_letters:
+        clear()
+        print('You already mentioned this letter!')
+        sleep(3)
+        continue
+    else: entered_letters.append(letter)
     animation('You.', True, print_game, word, know_letters, points)
     if word.count(letter) > 0:
         print('You guessed!')
         sleep(3)
         animation2(word, know_letters, letter)
-        if value is int: points += value
-        elif value == 'x': points *= word.count(letter)
-        else: points = 0
+        if value == 'x': points *= word.count(letter) + 1
+        else: points += value  
     else: 
         print('You didn`t guess :(')
         sleep(3)
